f2e2c6dd368858f332bbc832b1468cf2517c6a05
will <wye139629@gmail.com>
2021-01-05 18:58:22 +0800

    finish user current position marker

--- src/App.js
+++ src/App.js
@@ -7,12 +7,40 @@ import LeafLet from "leaflet";
 
 function App() {
   const osmUrl = " https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  useEffect(() => {
-    let myMap = LeafLet.map("mapid").setView([25.03418, 121.564517], 15);
+  useEffect(async() => {
 
-    LeafLet.tileLayer(osmUrl, {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-    }).addTo(myMap);
+
+      navigator.geolocation.getCurrentPosition(function(position){
+        let center = [position.coords.latitude, position.coords.longitude]
+        let greenIcon = new LeafLet.Icon({
+          iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+          shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+          iconSize: [25, 41],
+          iconAnchor: [12, 41],
+          popupAnchor: [1, -34],
+          shadowSize: [41, 41]
+        });
+        let myMap = LeafLet.map("mapid",{
+         center : center,  // center point coordinates
+         zoom : 18 ,  // 0-18
+         attributionControl : true ,
+         zoomControl : true  ,  // Whether to show the-+ button
+       })
+          LeafLet.tileLayer(osmUrl, {
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+        }).addTo(myMap);
+        let marker = LeafLet.marker(center,{icon: greenIcon}).addTo(myMap).bindPopup("Your are here.").openPopup()
+
+      });
+
+
+
+
+
+
+
+    // console.log(myMap.getCenter())
+    // let marker = LeafLet.marker([]).addTo(myMap);
     return () => {
 
     }
