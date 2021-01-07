7e7bd1fe98e7a9bc8a7b18b5061f9405400be232
will <wye139629@gmail.com>
2021-01-07 15:49:03 +0800

    render city and district select option

--- src/App.js
+++ src/App.js
@@ -9,21 +9,57 @@ import "leaflet.markercluster/dist/leaflet.markercluster.js"
 
 function App() {
   const [maskData, setMaskData] = useState(null)
-  // const [map, setMap] = useState(null)
+  const [cities, setCities] = useState(null)
+  const [allDistricts, setAllDistricts] = useState(null)
+  const [districts, setDistricts] = useState([])
+  const [map, setMap] = useState(null)
+  const [today, setToday] = useState("")
+  const [day, setDay]= useState(0)
 
   useEffect(() => {
     async function fetchMaskData(params) {
       const response =  await fetch("https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json")
       const json = await response.json();
-      setMaskData(json)
+      setMaskData(json.features)
+
+      let cities = {}
+      let districts = {}
+      json.features.map((data)=>{
+        if(data.properties.county in cities !== true && data.properties.county !== ""){
+          cities[data.properties.county] = [data.geometry.coordinates[1], data.geometry.coordinates[0]]
+        }
+        for(let city in cities){
+          if(data.properties.county === city && data.properties.town in districts !== true){
+            districts[data.properties.town] = {
+              city: city,
+              location: [data.geometry.coordinates[1], data.geometry.coordinates[0]]
+            }
+          }
+          // console.log(data.properties)
+        }
+      })
+      setCities(cities)
+      setAllDistricts(districts)
+
+
     }
+    function getDate() {
+      const dateObj = new Date()
+      const year = dateObj.getFullYear()
+      const month = dateObj.getMonth()
+      const date = dateObj.getDate()
+      const day = dateObj.getDay()
+      const todayDate = `${year}/${month+1}/${date}`
+      setToday(todayDate)
+      setDay(day)
+    }
+    getDate()
     fetchMaskData()
   }, [])
 
   useEffect(() => {
     if(!maskData)return
-    // console.log(maskData)
-    const osmUrl = " https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    const osmUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       navigator.geolocation.getCurrentPosition(function(position){
         let center = [position.coords.latitude, position.coords.longitude]
         let greenIcon = new LeafLet.Icon({
@@ -46,18 +82,94 @@ function App() {
         LeafLet.marker(center,{icon: greenIcon}).addTo(myMap).bindPopup("Your are here.").openPopup()
         let markers = new LeafLet.markerClusterGroup().addTo(myMap)
 
-        maskData.features.map((data)=>{
-          // console.log(data.properties)
+        maskData.map((data)=>{
           markers.addLayer(LeafLet.marker([data.geometry.coordinates[1],data.geometry.coordinates[0]],{icon: greenIcon}).bindPopup("<h3>"+data.properties.name+"</h3>" + "<p>成人口罩數量:"+data.properties.mask_adult +"<br/>"+ "兒童口罩數量:"+data.properties.mask_child+"</p>"))
 
         })
         myMap.addLayer(markers)
+        setMap(myMap)
       });
   },[maskData])
 
+
+  function updateDistrictOption(options) {
+    setDistricts(options)
+  }
+
   return (
-    <div id="mapid"></div>
+    <div className="container">
+      <SearchPanel dateData = {{today, day}} areaData = {{cities, districts, allDistricts}} updateDistrictOption={updateDistrictOption}/>
+      <div id="mapid"></div>
+    </div>
   );
 }
+
+
+function SearchPanel(props) {
+
+  return(
+    <div className="search-panel">
+      <SearchArea dateData = {props.dateData} areaData = {props.areaData} updateDistrictOption={props.updateDistrictOption}/>
+    </div>
+  )
+}
+
+
+function SearchArea(props) {
+  const {today, day} = props.dateData
+  const {cities, districts, allDistricts} = props.areaData
+  const checkDay = {
+    1: "星期一",
+    2: "星期二",
+    3: "星期三",
+    4: "星期四",
+    5: "星期五",
+    6: "星期六",
+    7: "星期日"
+  }
+  const cityOption = []
+  for(let city in cities){
+    cityOption.push(<option value={city}>{city}</option>)
+  }
+
+  function cityChangeHandler(e) {
+    let districtOption =[]
+    document.querySelector("#district").disabled = false
+    for(let district in allDistricts){
+      if(allDistricts[district].city === e.target.value){
+        districtOption.push(<option value={district}>{district}</option>)
+      }
+    }
+
+    props.updateDistrictOption(districtOption)
+  }
+
+  return(
+    <div className="searchArea">
+      <div className="dateTime">
+        <div className="day">{checkDay[day]}</div>
+        <div className="date">{today}</div>
+      </div>
+      <form className="areaSelect">
+        <select id="cities" onChange={cityChangeHandler}>
+          <option value="">請選擇縣市</option>
+          {cityOption}
+        </select>
+        <select id="district" disabled>
+          <option value="">請選擇行政區</option>
+          {districts}
+        </select>
+      </form>
+    </div>
+  )
+}
+
+function AreaOption(props) {
+return(
+  <option></option>
+)
+}
+
+
 
 export default App;
